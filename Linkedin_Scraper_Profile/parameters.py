6e723c56c2100e926dd9a703684255100484d8dc
[Your <roma.sulimak@gmail.com>
2020-11-21 12:40:08 +0200

    Initial commit

---
+++ Linkedin_Scraper_Profile/parameters.py
@@ -0,0 +1,10 @@
+# search query to be executed
+company = 'Expertise Events'
+search_query = 'site:linkedin.com/in/ + Expertise Events" + "Marketing"'
+
+# file were scraped profile information will be stored
+file_name = 'results_file.csv'
+
+# login credentials
+linkedin_username = 'post'
+linkedin_password = 'password'
